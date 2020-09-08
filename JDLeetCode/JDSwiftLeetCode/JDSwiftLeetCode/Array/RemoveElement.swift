9f82f9e3bd7bf8e3bec9ac56dff8aa521516a102
anotherNight <another.night.1024@gmail.com>
2020-09-08 18:51:02 +0800

    feat: update

--- JDLeetCode/JDSwiftLeetCode/JDSwiftLeetCode/Array/RemoveElement.swift
+++ JDLeetCode/JDSwiftLeetCode/JDSwiftLeetCode/Array/RemoveElement.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+/**
+ 题目 #
+
+ Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+ Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+ The order of elements can be changed. It doesn’t matter what you leave beyond the new length.
+ 
+ 题目大意 #
+
+ 给定一个数组 nums 和一个数值 val，将数组中所有等于 val 的元素删除，并返回剩余的元素个数。
+ */
 struct RemoveElement: JDLeetCodeProtocol{
     func main() {
         var nums: [Int] = [0,0,1,1,1,2,2,3,3,4]
